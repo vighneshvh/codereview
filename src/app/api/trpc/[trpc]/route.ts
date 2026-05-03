31c187e205dc836b3551dae02b0db7ea02011aa6
vighnesh hegde <vighneshhegde4@gmail.com>
2026-05-03 23:52:56 +0530

    build fix

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,9 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
